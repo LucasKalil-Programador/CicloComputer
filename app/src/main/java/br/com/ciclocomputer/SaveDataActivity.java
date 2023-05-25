6584c7358a9d01633dea9b3e1640e8dce526e3f1
09Uno <bruno.a.oliveira.1209@gmail.com>
2023-05-25 19:56:36 -0300

    update

--- app/src/main/java/br/com/ciclocomputer/SaveDataActivity.java
+++ app/src/main/java/br/com/ciclocomputer/SaveDataActivity.java
@@ -1,21 +1,26 @@
 package br.com.ciclocomputer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
 
-public class SaveDataActivity extends AppCompatActivity {
+public class SaveDataActivity extends Fragment {
 
 
     private static ArrayList<SaveDataModel> data = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_save_data);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_save_data, container, false);
     }
 
     public static void addDataModel(SaveDataModel dataParam) {
