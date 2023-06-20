a242075ae670a7534f9d5285fc4c6299f037f63c
09Uno <bruno.a.oliveira.1209@gmail.com>
2023-06-20 14:37:58 -0300

    maps update

--- app/src/main/java/br/com/ciclocomputador/NavbarFragment.java
+++ app/src/main/java/br/com/ciclocomputador/NavbarFragment.java
@@ -1,5 +1,6 @@
 package br.com.ciclocomputador;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,45 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class NavbarFragment extends Fragment {
 
+    private ImageButton routeButton;
+    private ImageButton mapButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_navbar, container, false);
+        View view = inflater.inflate(R.layout.fragment_navbar, container, false);
+
+        routeButton = view.findViewById(R.id.route_button);
+        routeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRouteClick(v);
+            }
+        });
+
+        mapButton = view.findViewById(R.id.map_button);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMapClick(v);
+            }
+        });
+
+        return view;
     }
 
+    private void onRouteClick(View v) {
+        // Navegar para a página da rota
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void onMapClick(View v) {
+        // Navegar para a página do mapa
+        Intent intent = new Intent(getActivity(), MapsActivity.class);
+        startActivity(intent);
+    }
 }
